482653b97d247763b623efc6b0261b49c79e243a
samjmccormick <samjmccormick@gmail.com>
2024-09-25 14:20:43 -0700

    everything is working except the special defence element is giving null, need to figure that out

--- script.js
+++ script.js
@@ -7,14 +7,42 @@ const pokemonName = document.getElementById("pokemon-name");
 const pokemonId = document.getElementById("pokemon-id");
 const height = document.getElementById("height");
 const weight = document.getElementById("weight");
-const types = document.getElementById("types");
-const hp = document.getElementById("hp");
-const attack = document.getElementById("attack");
-const defence = document.getElementById("defence");
-const specialAttack = document.getElementById("special-attack");
-const specialDefence = document.getElementById("special-defence");
-const speed = document.getElementById("speed");
-
-const allValidPokemon = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
+const typesElement = document.getElementById("types");
 
 
+const allValidPokemon = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/";
+
+async function fetchData() {
+    try {
+        const res = await fetch(allValidPokemon + searchInput.value);
+        const data = await res.json();
+        showStats(data);
+    } catch (err) {
+        alert("Pokémon not found");
+    }
+};
+
+function showStats(data) {
+    const { sprites, stats, types } = data;
+    pokemonName.textContent = data.name.toUpperCase();
+    pokemonId.textContent = data.id;
+    height.textContent = data.height;
+    weight.textContent = data.weight;
+    for (let i = 0; i < stats.length; i++) {
+        if (document.getElementById(`${stats[i].stat.name}`)) {
+            document.getElementById(`${stats[i].stat.name}`).textContent = stats[i].base_stat
+        }
+    }
+  
+    for (let i = 0; i < types.length; i++) {
+        typesElement.innerHTML += `<span class="types">${types[i].type.name.toUpperCase()}</span>`
+    }
+
+    imgDiv.innerHTML = `<img src="${sprites.front_default}" alt="${data.name.toUpperCase()}" >`
+}
+
+
+searchBtn.addEventListener("click", () => {
+    typesElement.innerHTML = "";
+    fetchData()
+})
