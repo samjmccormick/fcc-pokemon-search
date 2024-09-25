d0aca011694b38733235ff1f326eaf3d20ec54d0
samjmccormick <samjmccormick@gmail.com>
2024-09-25 11:53:25 -0700

    completed HTML and most of CSS started JS, defined constants from HTML

---
+++ script.js
@@ -0,0 +1,20 @@
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
+
+const imgDiv = document.getElementById("img-wrapper");
+
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonId = document.getElementById("pokemon-id");
+const height = document.getElementById("height");
+const weight = document.getElementById("weight");
+const types = document.getElementById("types");
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defence = document.getElementById("defence");
+const specialAttack = document.getElementById("special-attack");
+const specialDefence = document.getElementById("special-defence");
+const speed = document.getElementById("speed");
+
+const allValidPokemon = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
+
+
